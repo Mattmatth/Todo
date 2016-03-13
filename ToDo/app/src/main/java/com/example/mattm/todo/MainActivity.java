30f2616d7fb40a444c606488d51da1437187c78e
matthieu peyrot <matthieu.peyrot@epitech.eu>
2016-03-13 20:44:29 +0000

    Milestones 6
    Add a listener to the list view that will remove a list item on a long
    press. However, there is one missing setting that may prevent your long
    clicks from registering. It is up to you to figure out what the missing
    setting is.

--- ToDo/app/src/main/java/com/example/mattm/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/example/mattm/todo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -28,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mListView = (ListView) findViewById(R.id.mylistview);
+        tasks= new ArrayList<Task>();
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                tasks.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
         ShowListtasks();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(onAddingListener());
@@ -35,85 +46,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private View.OnClickListener onAddingListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Dialog dialog = new Dialog(MainActivity.this);
-                dialog.setContentView(R.layout.add_task);
-                dialog.setTitle("Add a Task");
-                dialog.setCancelable(false);
-                EditText tasktext = (EditText) dialog.findViewById(R.id.tasktext);
-                View btnAdd = dialog.findViewById(R.id.btn_ok);
-                View btnCancel = dialog.findViewById(R.id.btn_cancel);
+            private View.OnClickListener onAddingListener() {
+                return new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final Dialog dialog = new Dialog(MainActivity.this);
+                        dialog.setContentView(R.layout.add_task);
+                        dialog.setTitle("Add a Task");
+                        dialog.setCancelable(false);
+                        EditText tasktext = (EditText) dialog.findViewById(R.id.tasktext);
+                        View btnAdd = dialog.findViewById(R.id.btn_ok);
+                        View btnCancel = dialog.findViewById(R.id.btn_cancel);
 
 
-                btnAdd.setOnClickListener(onConfirmListener(tasktext, dialog));
-                btnCancel.setOnClickListener(onCancelListener(dialog));
+                        btnAdd.setOnClickListener(onConfirmListener(tasktext, dialog));
+                        btnCancel.setOnClickListener(onCancelListener(dialog));
 
-                dialog.show();
+                        dialog.show();
+                    }
+                };
             }
-        };
-    }
 
-    private View.OnClickListener onConfirmListener(final EditText tasktext, final Dialog dialog) {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Task task = new Task(tasktext.getText().toString().trim(),false);
-                tasks.add(task);
-                adapter.notifyDataSetChanged();
-                dialog.dismiss();
+            private View.OnClickListener onConfirmListener(final EditText tasktext, final Dialog dialog) {
+                return new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Task task = new Task(tasktext.getText().toString().trim(), false);
+                        tasks.add(task);
+                        adapter.notifyDataSetChanged();
+                        dialog.dismiss();
+                    }
+                };
             }
-        };
-    }
 
-    private View.OnClickListener onCancelListener(final Dialog dialog) {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
+            private View.OnClickListener onCancelListener(final Dialog dialog) {
+                return new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                };
             }
-        };
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+                return true;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+
+                return super.onOptionsItemSelected(item);
+            }
+
+
+            private List<Task> genererTasks() {
+
+                tasks.add(new Task("Florent", false));
+                tasks.add(new Task("Kevin", false));
+                tasks.add(new Task("Logan", false));
+                tasks.add(new Task("Matthieu", true));
+                tasks.add(new Task("Willy", false));
+                return tasks;
+            }
+
+            private void ShowListtasks() {
+                tasks = genererTasks();
+                adapter = new TaskAdapter(MainActivity.this, tasks);
+                mListView.setAdapter(adapter);
+            }
+
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private List<Task> genererTasks(){
-        tasks= new ArrayList<Task>();
-        tasks.add(new Task("Florent", false));
-        tasks.add(new Task("Kevin", false));
-        tasks.add(new Task("Logan", false));
-        tasks.add(new Task("Matthieu",true));
-        tasks.add(new Task("Willy", false));
-        return tasks;
-    }
-
-    private void ShowListtasks(){
-        tasks = genererTasks();
-        adapter = new TaskAdapter(MainActivity.this, tasks);
-        mListView.setAdapter(adapter);
-    }
-
-}
